72992a66ae7baa8942c8ebe123fb1aca7a051156
krishpranav <krisna.pranav@gmail.com>
2021-03-15 17:02:03 +0530

    done changes

--- godork.php
+++ godork.php
@@ -34,6 +34,32 @@ define('CSE_TOKEN', 'partner-pub-2698861478625135:3033704849');
         }
 
     }
-    
+
+    function get_string($string, $start, $end){
+        $str = explode($start, $string);
+        $str = explode($end, $str[1]);
+        return $str[0];
+    }
+    function banner() {
+        print "Go Dork"
+    }
+    banner();
+    echo "# DORK: "; $dork = fgets(STDIN);
+    $i = 0;
+    while(true){
+        $headers = array();
+        $headers[] = 'Referer: https://cse.google.com/cse?cx='.CSE_TOKEN;
+        $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.'.rand(0000, 3333).'.169 Safari/537.36';
+        $getInfo = Curl("https://cse.google.com/cse.js?hpg=1&cx=".CSE_TOKEN, 0, $headers);
+        preg_match('#"cselibVersion": "(.*?)"#si', $getInfo[1], $csiLib); // CSILIB
+        preg_match('#"cx": "(.*?)"#si', $getInfo[1], $cx); // CX
+        preg_match('#"cse_token": "(.*?)"#si', $getInfo[1], $cseToken); // CSE TOKEN
+        preg_match('#"exp": \["(.*?)", "(.*?)"\]#si', $getInfo[1], $exp); // EXP
+        preg_match('#"resultSetSize": "(.*?)"#si', $getInfo[1], $rsz); // RSZ
+        $url = "https://cse.google.com/cse/element/v1?rsz=".$rsz[1]."&num=10&&start=".$i."&hl=en&source=gcsc&gss=.com&cselibv=".$csiLib[1]."&cx=".$cx[1]."&q=".rawurlencode($dork)."&safe=off&cse_tok=".$cseToken[1]."&exp=".$exp[1].",".$exp[2]."&callback=google.search.cse.api16950";
+        $json = Curl($url, 0, $headers);
+        preg_match_all('#"clicktrackUrl": "(.*?)"#si', $json[1], $trackUrl);
+        
+    }
 
 ?>
