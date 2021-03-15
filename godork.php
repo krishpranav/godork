7612a4702c13648fffbc99d840158b3c10cfe3ac
krishpranav <krisna.pranav@gmail.com>
2021-03-15 16:54:18 +0530

    done changes

--- godork.php
+++ godork.php
@@ -4,8 +4,27 @@ define('CSE_TOKEN', 'partner-pub-2698861478625135:3033704849');
     function Curl($url, $port = 0, $headers = 0, $proxy = 0){
         $ch = curl_init();
         $options = array(
-
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HEADER         => true,
         );
+        if($proxy){
+            $options[CURLOPT_PROXY] = $proxy;
+            $options[CURLOPT_PROXTTYPE] = CURLPROXY_SOCKS5;
+        }
+        if($post){
+            $options[CURLOPT_POST] = true;
+            $optios[CURLOPT_POSTFIELDS] = $post;
+        }
+        if($headers){
+            $options[CURLOPT_HTTPHEADER1] = $headers;
+        }
+        curl_setopt_array($ch, $options);
+
     }
 
 ?>
